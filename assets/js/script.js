ba9bd7057534e16496d5c9e6860a989dc32aa518
SoulOppen <arieloppenheimer@outlook.com>
2024-07-08 21:49:41 -0400

    rapair

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $(".text-body-secondary").click(function() {
-      var idBoton = $("#Rio").attr("id");
-      $("#detalles" + idBoton).toggle();
-    });
-
-
-    $(".btn-close").click(function() {
-        $(".detalles").show();
-    });
+$(document).ready(function () {
+  $(".text-body-secondary").click(function () {
+    var idBoton = $("this").attr("id");
+    $("#detalles" + idBoton).toggle();
   });
+
+
+  $(".btn-close").click(function () {
+    $(".detalles").hide();
+  });
+});
